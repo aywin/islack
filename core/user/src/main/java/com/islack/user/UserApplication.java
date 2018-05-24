2b7f490c01b299097d8a6f1db5b0b0c2405929ff
Yassine Benabbou <yassine@yassineb.com>
2018-05-24 17:00:15 +0000

    Change module name to user

--- core/user/src/main/java/com/islack/user/UserApplication.java
+++ core/user/src/main/java/com/islack/user/UserApplication.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
-import java.util.Map;
 
 
 @SpringBootApplication
