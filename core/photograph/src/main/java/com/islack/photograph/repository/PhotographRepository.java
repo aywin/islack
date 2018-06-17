8ec217ec3cfd6353082ae5f6602ed8a7f237b755
Yassine Benabbou <yassine@yassineb.com>
2018-06-17 17:50:26 +0100

    Change pageable query Group By clause

--- core/photograph/src/main/java/com/islack/photograph/repository/PhotographRepository.java
+++ core/photograph/src/main/java/com/islack/photograph/repository/PhotographRepository.java
@@ -14,12 +14,12 @@ public interface PhotographRepository extends JpaRepository<Photograph, Long> {
     List<Photograph> findByUsername(String username);
     Optional<Photograph> findByUsernameAndId(String username, Long id);
     @Query("SELECT p FROM Photograph p join p.tags t WHERE t.tag IN (SELECT t2 FROM Photograph p2 join p2.tags t2 WHERE p2.id = :photograph)" +
-            " AND p.id <> :photograph GROUP BY p ORDER BY COUNT(p) DESC")
+            " AND p.id <> :photograph GROUP BY p.id, p.credit, p.thumbnail, p.uri, p.createdDate, p.updatedDate, p.username ORDER BY COUNT(p) DESC")
     Page<Photograph> findRecommended(@Param("photograph") Long photograph, Pageable pageable);
     @Query("SELECT p FROM Photograph p join p.tags t WHERE t.tag IN (SELECT t2 FROM Photograph p2 join p2.tags t2 WHERE p2.id IN (:list))" +
-            " AND p.id NOT IN (:list) GROUP BY p ORDER BY COUNT(p) DESC")
+            " AND p.id NOT IN (:list) GROUP BY p.id, p.credit, p.thumbnail, p.uri, p.createdDate, p.updatedDate, p.username ORDER BY COUNT(p) DESC")
     Page<Photograph> findRecommended(@Param("list") List<Long> list, Pageable pageable);
-    @Query("SELECT p FROM Photograph p left join p.photographAccesses a GROUP BY p ORDER BY COUNT(a) DESC")
+    @Query("SELECT p FROM Photograph p left join p.photographAccesses a GROUP BY p.id, p.credit, p.thumbnail, p.uri, p.createdDate, p.updatedDate, p.username ORDER BY COUNT(a) DESC")
     Page<Photograph> findPopular(Pageable pageable);
     Page<Photograph> findByCategoriesSlug(String slug, Pageable pageable);
 }
